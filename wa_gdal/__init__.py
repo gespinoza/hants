f26aa5a1ccf077379a4248f736a5b914cb1d03ec
gespinoza <gespinoza@utexas.edu>
2017-11-16 17:55:56 +0100

    Single point added

--- wa_gdal/__init__.py
+++ wa_gdal/__init__.py
@@ -19,7 +19,8 @@ The only difference between the two options is the underlying library to
 process the geographic data. The HANTS algorithm is the same.
 
 Example 1:
-from hants import wa_gdal
+from hants.wa_gdal import *
+
 # Data parameters
 rasters_path = r'C:\example\data'
 name_format = 'PROBAV_S1_TOC_{0}_100M_V001.tif'
@@ -30,6 +31,7 @@ lonlim = [108.8605, 108.8902]
 cellsize = 0.00099162627
 nc_path = r'C:\example\ndvi_probav.nc'
 rasters_path_out = r'C:\example\output_rasters'
+
 # HANTS parameters
 nb = 365
 nf = 3
@@ -37,20 +39,23 @@ low = -1
 high = 1
 HiLo = 'Lo'
 fet = 0.05
-delta = 0.25
+delta = 0.1
 dod = 1
+
 # Run
-wa_gdal.run_HANTS(rasters_path, name_format,
-                  start_date, end_date, latlim, lonlim, cellsize, nc_path,
-                  nb, nf, HiLo, low, high, fet, dod, delta,
-                  4326, -9999.0, rasters_path_out)
+run_HANTS(rasters_path, name_format,
+          start_date, end_date, latlim, lonlim, cellsize, nc_path,
+          nb, nf, HiLo, low, high, fet, dod, delta,
+          4326, -9999.0, rasters_path_out)
+
 # Check fit
 point = [108.87, 11.47]
 ylim = [-1, 1]
-wa_gdal.plot_point(nc_path, point, ylim)
+plot_point(nc_path, point, ylim)
 
 Example 2:
-from hants import wa_gdal
+from hants.wa_gdal import *
+
 # Create netcdf file
 rasters_path = r'C:\example\data'
 name_format = 'PROBAV_S1_TOC_{0}_100M_V001.tif'
@@ -60,32 +65,40 @@ latlim = [11.4505, 11.4753]
 lonlim = [108.8605, 108.8902]
 cellsize = 0.00099162627
 nc_path = r'C:\example\ndvi_probav.nc'
-wa_gdal.create_netcdf(rasters_path, name_format, start_date, end_date,
-                      latlim, lonlim, cellsize, nc_path)
-# Run HANTS
+create_netcdf(rasters_path, name_format, start_date, end_date,
+              latlim, lonlim, cellsize, nc_path)
+
+# Run HANTS for a single point
 nb = 365
 nf = 3
 low = -1
 high = 1
 HiLo = 'Lo'
 fet = 0.05
-delta = 0.25
+delta = 0.1
 dod = 1
-wa_gdal.HANTS_netcdf(nc_path, nb, nf, HiLo, low, high, fet, dod, delta)
-# Check fit
+
 point = [108.87, 11.47]
+df = HANTS_singlepoint(nc_path, point, nb, nf, HiLo, low, high, fet,
+                       dod, delta)
+print df
+
+# Run HANTS
+HANTS_netcdf(nc_path, nb, nf, HiLo, low, high, fet, dod, delta)
+
+# Check fit
 ylim = [-1, 1]
-wa_gdal.plot_point(nc_path, point, ylim)
+plot_point(nc_path, point, ylim)
+
 # Export rasters
 rasters_path_out = r'C:\example\output_rasters'
-wa_gdal.export_tiffs(rasters_path_out, nc_path, name_format)
-
+export_tiffs(rasters_path_out, nc_path, name_format)
 """
 
 from .main import (run_HANTS, create_netcdf, HANTS_netcdf, HANTS,
-                   export_tiffs, plot_point)
+                   export_tiffs, HANTS_singlepoint, plot_point)
 
 __all__ = ['run_HANTS', 'create_netcdf', 'HANTS_netcdf', 'HANTS',
-           'export_tiffs', 'plot_point']
+           'export_tiffs', 'HANTS_singlepoint', 'plot_point']
 
 __version__ = '0.1'
